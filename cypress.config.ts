ca22a9c279b60b92373a33e6469a5e1df77b8391
Pushpak Chhajed <pushpak1300@gmail.com>
2024-06-30 21:49:28 +0530

    ci: add gitlab cloud config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
+	projectId: "ko5dq3",
 	video: false,
 	e2e: {
 		setupNodeEvents(_on, _config) {},
